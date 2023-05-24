24f63cc13be51107a39f693e028884dfd07c6f7b
Simonsihole <72442153+Simonsihole@users.noreply.github.com>
2023-05-24 16:39:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,40 @@
-const Eris = require("eris");
+onst Eris = require("eris");
 const { Configuration, OpenAIApi } = require("openai");
+const keepAlive = require("./server");
 require('dotenv').config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+  apiKey: "Open AI API Key",
+});
 const openai = new OpenAIApi(configuration);
 
-const bot = new Eris(process.env.DISCORD_BOT_TOKEN, {
-    intents: [
-        "guildMessages"
-    ]
+const bot = new Eris("BotToken", {
+  intents: [
+    "guildMessages"
+  ]
 });
 
-async function runCompletion (message) {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: message,
-        max_tokens: 200,
-    });
-    return completion.data.choices[0].text;
+async function runCompletion(message) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 200,
+  });
+  return completion.data.choices[0].text;
 }
 
-bot.on("ready", () => { 
-    console.log("Bot is connected and ready!"); 
+bot.on("ready", () => {
+  console.log("Bot is connected and ready!");
 });
 
 bot.on("error", (err) => {
-  console.error(err); 
+  console.error(err);
 });
 
 bot.on("messageCreate", (msg) => {
-    if(msg.content.startsWith("#")) {
-        runCompletion(msg.content.substring(1)).then(result => bot.createMessage(msg.channel.id, result));
-    } 
+  if (msg.content.startsWith("#")) {
+    runCompletion(msg.content.substring(1)).then(result => bot.createMessage(msg.channel.id, result));
+  }
 });
-
+keepAlive();
 bot.connect();
